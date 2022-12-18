c9fb4238a683ed151c9bed9a544ac7060523ab3d
Tim Bowman <tim@timbowman.com.au>
2022-12-18 14:24:46 +1000

    support for args

--- ascii-convert.py
+++ ascii-convert.py
@@ -1,18 +1,13 @@
-ascii = """114
-114
-114
-111
-99
-107
-110
-114
-110
-48
-49
-49
-51
-114
-"""
+# For converting ASCII into string
 
-for c in ascii.split():
-    print(chr(int(c)), end='')
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Converting ASCII to string")
+    parser.add_argument("-d", "--decode", help="Add ASCII chars to decode")
+    args = parser.parse_args()
+    ascii = args.decode
+    # ascii = """114 114 114 111 99 107 110 114 110 48 49 49 51 114
+    # """
+
+    for c in ascii.split():
+        print(chr(int(c)), end='')
--- binary-convert.py
+++ binary-convert.py
@@ -1,4 +1,4 @@
-# Python3 code to demonstrate working of
+
 # Converting binary to string
 # Using BinarytoDecimal(binary)+chr()
 # Defining BinarytoDecimal() function
@@ -12,30 +12,33 @@ def BinaryToDecimal(binary):
         i += 1
     return decimal
 
-
-# Driver's code
-# initializing binary data
-#
-bin_data = "01110010"
-# print binary data
-print("The binary value is:", bin_data)
-# initializing a empty string for
-# storing the string data
-str_data = " "
-# slicing the input and converting it
-# in decimal and then converting it in string
-for i in range(0, len(bin_data), 7):
-    # slicing the bin_data from index range [0, 6]
-    # and storing it as integer in temp_data
-    temp_data = int(bin_data[i : i + 7])
-    # passing temp_data in BinarytoDecimal() function
-    # to get decimal value of corresponding temp_data
-    decimal_data = BinaryToDecimal(temp_data)
-    # Decoding the decimal value returned by
-    # BinarytoDecimal() function, using chr()
-    # function which return the string corresponding
-    # character for given ASCII value, and store it
-    # in str_data
-    str_data = str_data + chr(decimal_data)
-# printing the result
-print("The Binary value after string conversion is:", str_data)
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Binary to String Converter")
+    parser.add_argument("-d", "--decode", help="Decode binary data to string")
+    args = parser.parse_args()
+    # Driver's code
+    # initializing binary data
+    bin_data = args.decode
+    # print binary data
+    print("The binary value is:", bin_data)
+    # initializing a empty string for
+    # storing the string data
+    str_data = " "
+    # slicing the input and converting it
+    # in decimal and then converting it in string
+    for i in range(0, len(bin_data), 7):
+        # slicing the bin_data from index range [0, 6]
+        # and storing it as integer in temp_data
+        temp_data = int(bin_data[i : i + 7])
+        # passing temp_data in BinarytoDecimal() function
+        # to get decimal value of corresponding temp_data
+        decimal_data = BinaryToDecimal(temp_data)
+        # Decoding the decimal value returned by
+        # BinarytoDecimal() function, using chr()
+        # function which return the string corresponding
+        # character for given ASCII value, and store it
+        # in str_data
+        str_data = str_data + chr(decimal_data)
+    # printing the result
+    print("The Binary value after string conversion is:", str_data)
